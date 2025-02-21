7c489d4914e9f5c80b08bf168767c0f1a6607c2e
yc531 <yoyoyang95@gmail.com>
2025-02-21 12:05:45 +0800

    week01-1

---
+++ week01-6.cpp
@@ -0,0 +1,14 @@
+/// week01-1.cpp
+///在瘋狂程設(練習模式)寫好程式碼,全選,Ctrl-C
+///CodeBlocks File-New-Empty 空白檔案,Ctrl-C
+#include <stdio.h> ///使用standard標準的input/output
+
+
+int main()
+{
+	int a,b; ///整數a,b
+	scanf("%d%d",&a,&b); ///讀入資料(小心"%d%d"後面｢逗號)
+	///兩個%d%d & &
+	printf("%d",a+b); ///印出資料,把答案印出來
+
+}
